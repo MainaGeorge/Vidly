e13192c9f2d62bd7115b990fd79d6a01ce2bef7d
MainaGeorge <nuriszka@gmail.com>
2020-07-20 12:31:49 +0200

    Added data annotations to customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,9 +1,13 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
