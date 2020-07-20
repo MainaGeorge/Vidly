c7534dd4db963c405fcf95b7cac5b8a750c831b2
MainaGeorge <nuriszka@gmail.com>
2020-07-20 11:15:58 +0200

    Adding Home and Movie controllers

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers;
+
+        public CustomersController()
+        {
+            _customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Airun"},
+                new Customer {Id = 1, Name = "AniaM"},
+                new Customer {Id = 1, Name = "Ellie"}
+            };
+        }
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        private List<Movie> _movies;
+        public MoviesController()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie() {Id = 1, Name = "Baby boss"},
+                new Movie() {Id = 2, Name = "Fast and Furious"},
+                new Movie() {Id = 3, Name = "The Matrix"}
+            };
+        }
+        public ActionResult Index()
+        {
+            return View(_movies);
+        }
+    }
+}
