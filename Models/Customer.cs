de240461e7ad26909a69b091b8ec93e1d656aa25
MainaGeorge <nuriszka@gmail.com>
2020-07-20 13:22:17 +0200

    Added birthdate to customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vidly.Models
@@ -9,6 +10,8 @@ namespace Vidly.Models
         [Required]
         [MaxLength(255)]
         public string Name { get; set; }
+
+        public DateTime? BirthDate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
