83e85ef42793737a559700dd1dbab2e613715e0a
MainaGeorge <nuriszka@gmail.com>
2020-07-22 13:13:54 +0200

    Adding automapper

--- DTOs/CustomerDTO.cs
+++ DTOs/CustomerDTO.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
 
-namespace Vidly.Models
+namespace Vidly.DTOs
 {
-    public class Customer
+    public class CustomerDto
     {
         public int Id { get; set; }
 
@@ -11,18 +12,12 @@ namespace Vidly.Models
         [MaxLength(255)]
         public string Name { get; set; }
 
-        [Display(Name = "Date of Birth")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         [Ensure18YearsOfAgeWithAnySubscription]
         public DateTime? BirthDate { get; set; }
 
-        [Display(Name = "Subscribe to newsletter?")]
         public bool IsSubscribedToNewsletter { get; set; }
 
-        public MembershipType MembershipType { get; set; }
-
         [Required(ErrorMessage = "Please select a subscription")]
-        [Display(Name = "Subscription")]
         public byte MembershipTypeId { get; set; }
     }
 }
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
 
 namespace Vidly
 {
@@ -12,6 +9,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
